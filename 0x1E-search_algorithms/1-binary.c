b64b84d6a24064beb94d8ec495a8756c3051e3a3
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-11 10:34:09 +0300

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,1 +1,49 @@
 #include "search_algos.h"
+
+/**
+  * binary_search - Searches for a value in a sorted array
+  *                 of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
+int binary_search(int *array, size_t size, int value) {
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        print(start, end, array);
+
+        if (array[mid] == value) {
+            return mid;
+        } else if (array[mid] < value) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+/**
+  * print - prints the values in an array
+  * @array: A pointer to the first element of the array to search.
+  * @start: index to start printing
+  * @end: last index of the array to print. 
+  * Return: void
+  * Description: Prints the [sub]array being searched after each change.
+  */
+
+void print(int start, int end, int *array){
+    for (int i = start; i <= end; i++){
+            printf("%d", array[i]);
+    }
+    printf("\n");
+}
